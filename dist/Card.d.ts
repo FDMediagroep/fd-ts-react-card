55cc30b866b9223af8915bb98ef98aa2edb7fccc
Willem Liu <willem_liu@hotmail.com>
2019-01-23 11:14:17 +0100

    Build release

---
+++ dist/Card.d.ts
@@ -0,0 +1,6 @@
+import { PureComponent } from "react";
+export default class Card extends PureComponent<any, any> {
+    render(): JSX.Element;
+}
+declare const GlobalStyles: import("styled-components").GlobalStyleComponent<{}, import("styled-components").DefaultTheme>;
+export { GlobalStyles as CardStyle };
